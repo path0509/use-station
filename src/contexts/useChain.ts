817bc7fd5effa0b0cc11e0ffe9697202b250c5b6
sim <sim@terra.money>
2020-03-23 14:44:20 +0900

    로컬 FCD를 사용하기 위한 브랜치

--- src/contexts/useChain.ts
+++ src/contexts/useChain.ts
@@ -8,7 +8,8 @@ export const Chains: {
   columbus: { name: 'columbus-3', hostname: 'fcd.terra.dev' },
   vodka: { name: 'vodka-0001', hostname: 'vodka-fcd.terra.dev' },
   soju: { name: 'soju-0013', hostname: 'soju-fcd.terra.dev' },
-  fitz: { name: 'fitz', hostname: 'fitz.terra.money:5562' }
+  fitz: { name: 'fitz', hostname: 'fitz.terra.money:5562' },
+  local: { name: 'local', hostname: 'local.terra.money:0000' }
 }
 
 export default (initial?: ChainKey): ChainConfig => {
@@ -24,7 +25,7 @@ export default (initial?: ChainKey): ChainConfig => {
     set(initial ?? 'columbus')
   }, [initial])
 
-  const list: ChainKey[] = ['columbus', 'vodka', 'soju']
+  const list: ChainKey[] = ['columbus', 'vodka', 'soju', 'local']
 
   return { current, list, set }
 }
