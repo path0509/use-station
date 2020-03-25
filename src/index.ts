90a6d9f79f0d4dcf1c8c1ab57112787d168cd96f
sim <sim@terra.money>
2020-03-25 16:36:52 +0900

    fix: Stop to use LanguageDetector

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
+// import LanguageDetector from 'i18next-browser-languagedetector'
 import en from './lang/en'
 import zh from './lang/zh.json'
 import fr from './lang/fr.json'
 import ko from './lang/ko.json'
 
 i18n
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
